a9502b276e9a73fbb280164b482b7b05d3b13eda
msknatalie <cgs.korneeva@gmail.com>
2021-11-15 15:00:29 +0300

    add splice (line 20)

--- my-app/src/App.tsx
+++ my-app/src/App.tsx
@@ -1,9 +1,11 @@
-import React from "react";
+import React, { Component } from "react";
 import logo from "./logo.svg";
 import Graphic from "./Graphic";
 import "./App.css";
 
-function App() {
+class App extends Component {
+
+  render() {
   return (
     <div className="App">
         <h1>Welcome</h1>
@@ -11,11 +13,11 @@ function App() {
           title="the First Graphic"
           verticalLine={[10, 0, 10, 200]}
           horizontalLine={[200, 200, 10, 200]}
-          pointsRed="10,200 30,100 60,40 80,120 120,140 200,180"
-          pointsGreen="10,200 40,180 80,80 120,100 150,140 200,120"
+          values1={[20, 70, 50, 80, 10]}
         />
     </div>
   );
+}
 }
 
 export default App;
--- my-app/src/Graphic.tsx
+++ my-app/src/Graphic.tsx
@@ -5,46 +5,42 @@ import App from "./App";
 import { isThisTypeNode } from "typescript";
 
 interface IMyProps {
-  title: string;
-  verticalLine: number[];
-  horizontalLine: number[];
-  pointsRed: string;
-  pointsGreen: string;
+  title: string; // The name of the Graphic
+  verticalLine: number[]; // The x-axis and y-axis coordinates of the start and of the end of the line
+  horizontalLine: number[]; // The x-axis and y-axis coordinates of the start and of the end of the line
+  values1: number[]; // The list of points that makes up the polyline. All coordinate values are in the user coordinate system.
+  // newValues1: number[]
 }
 interface IMyState {
+  newValues1: string;
 }
 
 class Graphic extends Component<IMyProps, IMyState> {
-
   render() {
+    let newValues1 = this.props.values1.splice(' ').toString();
+    console.log(newValues1);
+
     return (
       <div className="Graphic">
-        <h1>{this.props.title}</h1>  {/* the name of the Graphic */}
+        <h1>{this.props.title}</h1>
         <div>
           <svg height="400" width="400">
             <line
-              x1={this.props.verticalLine[0]} // The x-axis coordinate of the start of the line
-              y1={this.props.verticalLine[1]} // The y-axis coordinate of the start of the line.
-              x2={this.props.verticalLine[2]} // The x-axis coordinate of the end of the line.
-              y2={this.props.verticalLine[3]} // The y-axis coordinate of the end of the line.
+              x1={this.props.verticalLine[0]}
+              y1={this.props.verticalLine[1]}
+              x2={this.props.verticalLine[2]}
+              y2={this.props.verticalLine[3]}
             />
             <text x="20" y="220" fill="black">
               График
             </text>
             <line
-              x1={this.props.horizontalLine[0]} // The x-axis coordinate of the start of the line
-              y1={this.props.horizontalLine[1]} // The y-axis coordinate of the start of the line.
-              x2={this.props.horizontalLine[2]} // The x-axis coordinate of the end of the line.
-              y2={this.props.horizontalLine[3]} // The y-axis coordinate of the end of the line.
+              x1={this.props.horizontalLine[0]}
+              y1={this.props.horizontalLine[1]}
+              x2={this.props.horizontalLine[2]}
+              y2={this.props.horizontalLine[3]}
             />
-            <polyline
-            className="lineRed"
-              points={this.props.pointsRed} // The list of points that makes up the polyline. All coordinate values are in the user coordinate system.
-            />
-            <polyline
-            className="lineGreen"
-              points={this.props.pointsGreen} // The list of points that makes up the polyline. All coordinate values are in the user coordinate system.
-            />
+            <polyline className="lineRed" points={this.state.newValues1} />
           </svg>
         </div>
       </div>
